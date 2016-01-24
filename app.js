f6f9aeafdbc005d19d86ce6b06c27f562aaf9ae0
iamwindel <windel@gmail.com>
2016-01-24 21:12:47 +0800

    popup modal
    show modal project

--- app.js
+++ app.js
@@ -17,20 +17,10 @@ angular.module('myApp', [
 
 .controller('navigationCtrl', ['$scope','$route','$window', function($scope, $route, $window) {
 	$scope.$route = $route;	
-  $scope.aaa = 'aaaaaa';
 }])
 
 .directive('gotop', ['$window','$route',function($window,$route) {
-  return function(scope, element, attrs) {   
-      element.bind('click',function(){
-        $window.scrollTo(0,0);
-      })
-
-      $(document).ready(function(){
-        console.log("ready");
-        setTimeout(function() {animateHome();}, 500);              
-      })
-
+  return function(scope, element, attrs) {
       angular.element($window).bind('scroll', function() {
         var scrollOffset = this.pageYOffset;
         if (scrollOffset >= 100) {
@@ -38,62 +28,11 @@ angular.module('myApp', [
         } else {
           element.hide();
         };
-        animateHome();      
       })
 
-      function animateHome(){
-        if ($route.current.activetab != 'home') return;
+      element.bind('click',function(){
+        $window.scrollTo(0,0);
+      })
 
-        var col1 = $("#homeColumn1");
-        var col2 = $("#homeColumn2");
-        var col3 = $("#homeColumn3");        
-        var featured = $('#featured2');
-        var proj1 = $('#proj1');
-
-        if (col2.hasClass('homeColumn2Start') ) {
-          col2.removeClass('homeColumn2Start').addClass('endTransMid');
-          col3.removeClass('homeColumn3Start').addClass('endTransFast');            
-        }
-        if (col1.hasClass('homeColumn1Start') ) {
-          
-          var isHomeColumn1 = (window.pageYOffset >= 100)
-          if (isHomeColumn1) {
-            col1.removeClass('homeColumn1Start').addClass('endTransSlow');
-            setTimeout(function() {$("#noi").removeClass('disappear').addClass('appearSlow');}, 800);            
-          }
-        }
-
-        if (featured.hasClass('featuredStart')) {
-          var isReadyFeature = noiTools.isElementInViewport('featuredMarker');
-          if (isReadyFeature){
-            featured.removeClass('featuredStart').addClass('endTransFast');            
-          }
-        }
-
-        if (proj1.hasClass('proj1Start') )
-        {
-          var isReadyProject = noiTools.isElementInViewport('proj4');
-          if (isReadyProject){
-
-            var proj2 = $('#proj2');
-            var proj3 = $('#proj3');
-            var proj4 = $('#proj4');
-            var proj5 = $('#proj5');
-            var proj6 = $('#proj6');
-            var proj7 = $('#proj7');
-            var proj8 = $('#proj8');          
-
-            proj1.removeClass('proj1Start').addClass('endTransMid');
-            proj2.removeClass('proj2Start').addClass('endTransSlow');
-            proj3.removeClass('proj3Start').addClass('endTransMid');
-            proj4.removeClass('proj4Start').addClass('endTransFast');
-            proj5.removeClass('proj5Start').addClass('endTransFast');
-            proj6.removeClass('proj6Start').addClass('endTransMid');
-            proj7.removeClass('proj7Start').addClass('endTransFast');
-            proj8.removeClass('proj8Start').addClass('endTransSlow');
-          }
-        }
-      }
-
-    };  
+    };
 }])
--- home/home.js
+++ home/home.js
@@ -13,21 +13,105 @@ angular.module('myApp.home', ['ngRoute'])
 .controller('HomeCtrl', ['$scope','$timeout','$window',function($scope, $timeout, $window) {
 }])
 
-/*.directive('scrollAnimate', function() {
-	return {
-		restrict: 'A',
-	    link: function (scope, element, attrs) {
-	        var raw = element[0];
-	        console.log('loading directive');
-	            
-	        element.bind('scroll', function () {
-	            console.log('in scroll');
-	            console.log(raw.scrollTop + raw.offsetHeight);
-	            console.log(raw.scrollHeight);
-	            if (raw.scrollTop + raw.offsetHeight > raw.scrollHeight) {
-	                scope.$apply(attrs.scrolly);
-	            }
-	        });
-	    };
-	};
-);*/
+
+.directive('homer', ['$window','$route',function($window,$route) {
+  return function(scope, element, attrs) {   
+
+      animateStart();
+
+      $(document).ready(function() {
+        $(".projthumb").on("click", function() {
+          var self = this;
+           $('#imagepreview').attr('src', $(self).attr('href'));
+           $('#imagemodal').modal('show');         
+           return false;
+        });
+      });
+
+      function animateStart(){
+      	//console.log("home.js bindings: animateStart");
+      	initializeAnimation();
+
+        setTimeout(function() {
+        	animateHome();
+			angular.element($window).bind('scroll.home', function() {
+				animateHome();
+			})        	
+        }, 500);
+      }
+
+      function initializeAnimation(){
+        $("#noi").addClass("disappear");
+        $("#homeColumn1").addClass("homeColumn1Start");
+        $("#homeColumn2").addClass("homeColumn2Start");
+        $("#homeColumn3").addClass("homeColumn3Start");
+		    $("#featured2").addClass("featuredStart");
+		    $("#featured2").addClass("featuredStart");
+        $("#proj1").addClass('proj1Start');
+        $("#proj2").addClass('proj2Start');
+        $("#proj3").addClass('proj3Start');
+        $("#proj4").addClass('proj4Start');
+        $("#proj5").addClass('proj5Start');
+        $("#proj6").addClass('proj6Start');
+        $("#proj7").addClass('proj7Start');
+        $("#proj8").addClass('proj8Start');      	
+      }
+
+      function animateHome(){
+        var col1 = $("#homeColumn1");
+        var col2 = $("#homeColumn2");
+        var col3 = $("#homeColumn3");        
+        var featured = $('#featured2');
+        var proj1 = $('#proj1');
+        //console.log("home.js  window scroll.home called");
+
+        if (col2.hasClass('homeColumn2Start') ) {
+          col2.removeClass('homeColumn2Start').addClass('endTransMid');
+          col3.removeClass('homeColumn3Start').addClass('endTransFast');            
+        }
+        if (col1.hasClass('homeColumn1Start') ) {          
+          var isHomeColumn1 = (window.pageYOffset >= 100)
+          if (isHomeColumn1) {
+            col1.removeClass('homeColumn1Start').addClass('endTransSlow');
+            setTimeout(function() {$("#noi").removeClass('disappear').addClass('appearSlow');}, 800);            
+          }
+        }
+
+        if (featured.hasClass('featuredStart')) {
+          var isReadyFeature = noiTools.isElementInViewport('featuredMarker');
+          if (isReadyFeature){
+            featured.removeClass('featuredStart').addClass('endTransFast');            
+            window.scrollBy(0, 400);
+          }
+        }
+
+        if (proj1.hasClass('proj1Start') )
+        {
+          var isReadyProject = noiTools.isElementInViewport('proj4');
+          if (isReadyProject){
+
+            var proj2 = $('#proj2');
+            var proj3 = $('#proj3');
+            var proj4 = $('#proj4');
+            var proj5 = $('#proj5');
+            var proj6 = $('#proj6');
+            var proj7 = $('#proj7');
+            var proj8 = $('#proj8');          
+
+            proj1.removeClass('proj1Start').addClass('endTransMid');
+            proj2.removeClass('proj2Start').addClass('endTransSlow');
+            proj3.removeClass('proj3Start').addClass('endTransMid');
+            proj4.removeClass('proj4Start').addClass('endTransFast');
+            proj5.removeClass('proj5Start').addClass('endTransFast');
+            proj6.removeClass('proj6Start').addClass('endTransMid');
+            proj7.removeClass('proj7Start').addClass('endTransFast');
+            proj8.removeClass('proj8Start').addClass('endTransSlow');
+
+            angular.element($window).unbind('scroll.home');
+            
+          }
+        }
+      }
+
+    };  
+}])
