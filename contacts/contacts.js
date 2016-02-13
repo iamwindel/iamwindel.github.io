daf22a5ed0eaf8ff66ac1f9ceb8e28555c246e86
iamwindel <windel@gmail.com>
2016-02-13 16:31:33 +0800

    finalize Iamwindel.com
    Thank you Lord for making this possible

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -9,29 +9,36 @@ angular.module('myApp.contacts', ['ngRoute'])
   });
 }])
 
-.controller('ContactsCtrl', ['$scope','$route','$http',function($scope, $http) {
+.controller('ContactsCtrl', ['$scope','$route','$http',function($scope, $route, $http) {
 /*	$scope.fullname = '';
 	$scope.email = '';
 	$scope.phone = '';
 	$scope.message = '';*/
 	$scope.pageTitle = 'contacts';
 	$scope.contact = {};
-	$scope.capcha = '';
-	$scope.captcha1 = 15;
-	$scope.captcha2 = 2
+
+	$scope.captcha1 = randomNumber(10, 15); // 15
+	$scope.captcha2 = randomNumber(1, 5); //2
+
+    function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
 	//... TODO: try not to pass the scope
-	$scope.submitForm = function() {		
-		$http({
-			method	: 'POST',
-			url 	: 'https://getsimpleform.com/messages?form_api_token=24822f3f3891f1dbce3c4bb4b69f0590', 
-			data	: $scope.contact,
-			//type 	: 'jsonp', // not needed
-			headers	: {'Content-Type': 'application/x-www-form-urlencoded'}
-		})
-		.success(function(data){
-			alert(data);
-		});
+	$scope.submitForm = function() {
+		debugger;
+		if ($scope.captcha1 + $scope.captcha2 == $scope.captcha ) {		
+			$http({
+				method	: 'POST',
+				url 	: 'https://getsimpleform.com/messages?form_api_token=24822f3f3891f1dbce3c4bb4b69f0590', 
+				data	: $scope.contact,
+				//type 	: 'jsonp', // not needed
+				headers	: {'Content-Type': 'application/x-www-form-urlencoded'}
+			})
+			.success(function(data){
+				alert("Message submitted. Thank you!");
+			});
+		}
 	};
 
 }]);
